323019c0638f8c6ebe304ecda0e633ae55ce13d7
blaketharrison <blaketharrison@gmail.com>
2018-09-27 22:25:45 -0500

    removing tabs page

--- tictactoe/src/app/app.module.ts
+++ tictactoe/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { GamePage } from '../pages/game/game';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,7 +13,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    TabsPage,
     GamePage
   ],
   imports: [
@@ -25,7 +23,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    TabsPage,
     GamePage
   ],
   providers: [
